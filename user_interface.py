34290adf3a17d5c352eb162dc64dce7ae5b99632
roshawn <roshawnwright@outlook.com>
2023-12-23 19:22:45 -0500

    Update Final

--- user_interface.py
+++ user_interface.py
@@ -5,17 +5,15 @@ from pathlib import Path
 sg.theme('BluePurple')
 
 
-# Define the layout
 layout = [
 	[sg.Image(filename='logo.png', size=(800, 600))],
 	[sg.Text('Click browse and select the file you want to remove or extract Exif data', font=("Any 12"))],
 	[sg.FileBrowse(target='-FILE-', file_types=(("Image Files", "*.png *.jpg *.jpeg"),)), 
-	sg.InputText(key='-FILE-')], #sg.InputText(size=(32, 1))],
+	sg.InputText(key='-FILE-')],
 	[sg.Button("Remove Data", button_color=('white', 'green')),sg.Button("Extract Data", button_color=('white', 'blue')),
 	sg.Cancel(button_color=('white', 'red'))],
 ]
 
-# Create the window
 window = sg.Window('Exif Data Remover', layout, element_justification='center', font=("Times New Roman", 16), size=(1100, 900))
 
 
@@ -23,19 +21,18 @@ while True:
 	event, values = window.read()
 	if event == sg.WINDOW_CLOSED or event == 'Cancel':
 		break
-	elif event == 'Remove Data':
-		file_path = values['-FILE-']
-		exif = ExifTag(Path(file_path))#instance of ExifTag
+	file_path = values.get('-FILE-')
+	if event == 'Remove Data':
+		exif = ExifTag(Path(file_path))
 		if file_path:
 			result = exif.remove_data()
 			if result is True:
 				sg.popup('Data removed successfully!', title='Success', background_color='cyan', text_color='white', auto_close=True, auto_close_duration=5)
 			else:
 				sg.popup_error(f'Error: {result}', title='Error', background_color='red', text_color='white')
-	elif event == 'Extract Data':
-		file_path = values['-FILE-']
+	if event == 'Extract Data':
 		if file_path:
-			exif = ExifTag(Path(file_path)) #instance of ExifTag
+			exif = ExifTag(Path(file_path))
 			data = exif.extract_data()
 			if data:
 				exif.save(data)
